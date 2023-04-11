f6df2ee575427c9c92d5de9220a77378192154b4
Hasan Mahmud <hmahmud01@gmail.com>
2023-04-11 17:55:40 +0800

    azra report

--- source/routes/agentReport.js
+++ source/routes/agentReport.js
@@ -4,6 +4,7 @@ import controller from '../controllers/agentReport.js';
 const agentReportRoutes = express.Router();
 
 agentReportRoutes.get('/agentreport', controller.agentReport);
+agentReportRoutes.get('/agentprofilereport/:id', controller.agentProfileReport);
 agentReportRoutes.get('/agentbalancecheck/:id', controller.agentBalance);
 agentReportRoutes.get('/agentdues/:id', controller.agentDues);
 agentReportRoutes.get('/agentearning/:id', controller.agentEarning);
